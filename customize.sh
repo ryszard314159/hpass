f4898a8c382de7e0f070f07403a23f978981c608
Ryszard Czerminski <ryszard@czerminski.net>
2022-11-23 11:46:28 -0500

    seems to be in sync now, replaced example with demo.html

--- customize.sh
+++ customize.sh
@@ -5,7 +5,7 @@ function usage() {
     echo
     echo "Usage example:"
     echo
-    echo "./customize.sh --sekret SEKRET --prefix PREFIX --length 8 < template.html > demo.html"
+    echo "./customize.sh --sekret SEKRET --prefix PREFIX --length 8 --burnin 9 < template.html > demo.html"
     echo
     echo SEKRET - can be any string
     echo "PREFIX - use one (or more) from ${special} to satisfy picky sites"
@@ -36,6 +36,11 @@ while [[ $# -gt 0 ]]; do
       shift # past argument
       shift # past value
       ;;
+    -b|--burnin)
+      BURNIN="$2"
+      shift # past argument
+      shift # past value
+      ;;
     -*|--*)
       echo "Unknown option $1"
       exit 1
@@ -55,5 +60,5 @@ sed -e '/Password Generator/d' \
   -e '/<label>Sekret:<\/label>/d' -e '/<input id="sekret"/d' -e "s/args.sekret = .*$/args.sekret = '${SEKRET}'/" \
   -e '/<label>Prefix:<\/label>/d' -e '/<input id="prefix"/d' -e "s/args.prefix = .*$/args.prefix = '${PREFIX}'/" \
   -e '/<label>Length:<\/label>/d' -e '/<input id="length"/d' -e "s/args.length = .*$/args.length = ${LENGTH}/" \
-  -e '/<label>Spice:<\/label>/d' -e '/<input id="spice"/d' -e "s/args.spice = .*$/args.spice = ${SPICE}/" \
+  -e '/<label>Burnin:<\/label>/d' -e '/<input id="burnin"/d' -e "s/args.burnin = .*$/args.burnin = ${BURNIN}/" \
   -e '/<hr style/d' -e '/passwords/d'
