a06308534f9f807267cc8825ddf2b8b2e8a91793
Ryszard Czerminski <ryszard@czerminski.net>
2023-02-15 14:57:35 -0500

    streamlined index.html

--- core/main.js
+++ core/main.js
@@ -20,52 +20,99 @@
 
 // import { ArgumentParser,  ArgumentDefaultsHelpFormatter} from "argparse";
 // const ArgumentParser = require('argparse').ArgumentParser;
-const { ArgumentParser,  ArgumentDefaultsHelpFormatter} = require('argparse/argparse.js');
-const assert = require('assert');
-const lib = require("./lib.js")
+const {
+  ArgumentParser,
+  ArgumentDefaultsHelpFormatter,
+} = require("argparse/argparse.js");
+const assert = require("assert");
+const lib = require("./lib.js");
 // import { getPass } from "./lib.js" // SyntaxError: Cannot use import statement outside a module
 
-const DESCRIPTION = 'Seeded password generator - generates password from hint and salt; \
+const DESCRIPTION =
+  "Seeded password generator - generates password from hint and salt; \
 it is augmented by one special character (pepper), one lower and upper char and a digit. \
-NOTE: defaults for salt, pepper and plength can be overwritten by environment variables'
+NOTE: defaults for salt, pepper and length can be overwritten by environment variables";
 
 function get_parser() {
   // import {ArgumentParser,  ArgumentDefaultsHelpFormatter} from "argparse";
   const parser = ArgumentParser({
     description: DESCRIPTION,
-    formatter_class: ArgumentDefaultsHelpFormatter
+    formatter_class: ArgumentDefaultsHelpFormatter,
   });
   // parser.add_argument('-v', '--version', { action: 'version', version });
-  parser.add_argument('hint', {help: "password hint; '' generates random passwd"});
-  parser.add_argument('-p', '--pepper', { help: 'punctuation pepper to use',
-         default: typeof process.env.PEPPER === 'undefined' ? '!' : process.env.PEPPER });
-  parser.add_argument('-s', '--salt', { help: 'hint augmentation',
-         default: typeof process.env.SALT === 'undefined' ? 'SALT' : process.env.SALT });
-  parser.add_argument('-b', '--burnin', { help: 'discard cycles', type: 'int', default: 0});
-  parser.add_argument('-L', '--plength', { help: 'password length', type: 'int',
-         default: typeof process.env.PLENGTH === 'undefined' ? 15 : process.env.PLENGTH });
-  parser.add_argument('-u', '--unicode', { help: 'use ALL unicode chars', action: "store_true"});
-  parser.add_argument('-r', '--letters', { help: 'use ascii letters', action: "store_true"});
-  parser.add_argument('-t', '--digits', { help: 'use digits', action: "store_true"});
-  parser.add_argument('-n', '--punctuation', { help: 'use punctuation', action: "store_true"});
-  parser.add_argument('-f', '--no-shuffle', { help: 'dont shuffle final string', action: "store_true"});
-  parser.add_argument('-a', '--random', { help: 'create random password', action: "store_true"});
-  parser.add_argument('-d', '--debug', { help: 'debug on', action: "store_true"});
-  parser.add_argument('-v', '--verbose', { help: 'verbose output', action: "store_true"});
+  parser.add_argument("hint", {
+    help: "password hint; '' generates random passwd",
+  });
+  parser.add_argument("-p", "--pepper", {
+    help: "punctuation pepper to use",
+    default:
+      typeof process.env.PEPPER === "undefined" ? "!" : process.env.PEPPER,
+  });
+  parser.add_argument("-s", "--salt", {
+    help: "hint augmentation",
+    default:
+      typeof process.env.SALT === "undefined" ? "SALT" : process.env.SALT,
+  });
+  parser.add_argument("-b", "--burnin", {
+    help: "discard cycles",
+    type: "int",
+    default: 0,
+  });
+  parser.add_argument("-L", "--length", {
+    help: "password length",
+    type: "int",
+    default:
+      typeof process.env.LENGTH === "undefined" ? 15 : process.env.LENGTH,
+  });
+  parser.add_argument("-u", "--unicode", {
+    help: "use ALL unicode chars",
+    action: "store_true",
+  });
+  parser.add_argument("-r", "--letters", {
+    help: "use ascii letters",
+    action: "store_true",
+  });
+  parser.add_argument("-t", "--digits", {
+    help: "use digits",
+    action: "store_true",
+  });
+  parser.add_argument("-n", "--punctuation", {
+    help: "use punctuation",
+    action: "store_true",
+  });
+  parser.add_argument("-f", "--no-shuffle", {
+    help: "dont shuffle final string",
+    action: "store_true",
+  });
+  parser.add_argument("-a", "--random", {
+    help: "create random password",
+    action: "store_true",
+  });
+  parser.add_argument("-d", "--debug", {
+    help: "debug on",
+    action: "store_true",
+  });
+  parser.add_argument("-v", "--verbose", {
+    help: "verbose output",
+    action: "store_true",
+  });
   // let args =  parser.parse_args()
   // if (args.debug) console.dir(args);
-  return parser
+  return parser;
 }
 
 let parser = get_parser();
 // let args, passwd;
-if (typeof process.env.BUILTIN !== 'undefined' || typeof window !== 'undefined') {
+if (
+  typeof process.env.BUILTIN !== "undefined" ||
+  typeof window !== "undefined"
+) {
   // get args from window s
-  print(`DEBUG: env.BUILTIN= ${process.env.BUILTIN}`)
-  args = parser.parse_args([$('#hint').val(), '-v'])
+  print(`DEBUG: env.BUILTIN= ${process.env.BUILTIN}`);
+  args = parser.parse_args([$("#hint").val(), "-v"]);
 } else {
   // get args from command line
-  args = parser.parse_args()
+  args = parser.parse_args();
 }
 if (args.debug) console.dir(args);
 
